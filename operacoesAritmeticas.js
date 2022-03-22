e97c33fa6dda3e52160b95042cba55ae2314ae31
Carolina Ariadne <c.ariadneadv@gmail.com>
2022-03-22 16:22:41 -0300

    Exercicio

---
+++ operacoesAritmeticas.js
@@ -0,0 +1,11 @@
+let num1 = 10;
+let num2 = 5;
+
+
+console.log( 'O resultado da soma é ' + (num1 + num2));
+
+console.log( 'O resultado da multiplicação é ' + (num1 * num2));
+
+console.log( 'O resultado da substração é ' + (num1 - num2));
+
+console.log( 'O resultado da divisão é ' + (num1 / num2));
