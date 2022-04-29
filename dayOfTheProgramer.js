341dc54f3cac4324cbf48ff8973868f20231127f
Carolina Ariadne <c.ariadneadv@gmail.com>
2022-04-29 13:15:34 -0300

    Ajustes para passar em todos testes

--- dayOfTheProgramer.js
+++ dayOfTheProgramer.js
@@ -1,24 +1,30 @@
 let arrayBissexto = [31, 29, 31, 30, 31, 30, 31, 31];
 let arrayNaoBissexto = [31, 28, 31, 30, 31, 30, 31, 31];
 let dayProgramerYear = 256;
-let year = 1988;
+let year = 2022;
 let anoBissexto = false;
 
-function verifyDateBissexto(y) {
-  if (year < 1700 || year > 2700) {
+function verifyDateInvalide(y) {
+  if (y < 1700 || y > 2700) {
     console.log("O ano não é válido, digite um ano entre 1700 e 2700");
-  }
-  if (year >= 1700 && year <= 1917) {
-    if (year % 4 === 0) {
-      anoBissexto = true;
-    } else if (year > 1917)
-      if ((y % 4 === 0 && y % 100 !== 0) || y % 400 === 0) {
-        anoBissexto = true;
-      }
+  } else {
+    verifyDateBissexto(year);
+    verifyDateProgramer();
   }
 }
 
-verifyDateBissexto(year);
+verifyDateInvalide(year);
+
+function verifyDateBissexto(y) {
+  if (y >= 1700 && y <= 1917) {
+    if (year % 4 === 0) {
+      anoBissexto = true;
+    }
+  } else if (y > 1917)
+    if ((y % 4 === 0 && y % 100 !== 0) || y % 400 === 0) {
+      anoBissexto = true;
+    }
+}
 
 function verifyDateProgramer() {
   if (anoBissexto) {
@@ -31,7 +37,7 @@ function verifyDateProgramer() {
     console.log(`${dayProgramer}.09.${year}`);
   }
 
-  if (!anoBissexto && year >= 1700) {
+  if (!anoBissexto) {
     const sumDaysNaoBissexto = arrayNaoBissexto.reduce(
       (acumulador, elemento) => acumulador + elemento
     );
@@ -39,6 +45,5 @@ function verifyDateProgramer() {
 
     console.log(`${dayProgramer2}.09.${year}`);
   }
-}
+};
 
-verifyDateProgramer();
