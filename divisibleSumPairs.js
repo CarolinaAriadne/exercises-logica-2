efafe96d308152a48ba75240ddb133b03184a965
Carolina Ariadne <c.ariadneadv@gmail.com>
2022-05-14 22:56:22 -0300

    Exercicio

---
+++ divisibleSumPairs.js
@@ -0,0 +1,18 @@
+let numbers = [1, 3, 2, 6, 1, 2];
+
+let divisorNumber = 3;
+
+function divisibleSumPairs(n, d) {
+  let quantity = 0;
+
+  for (let esquerda = 0; esquerda < n.length; esquerda += 1) {
+    for (let direita = esquerda + 1; direita < n.length; direita += 1) {
+      if ((n[esquerda] + n[direita]) % d === 0) {
+        quantity += 1;
+      }
+    }
+  }
+  console.log(quantity);
+}
+
+divisibleSumPairs(numbers, divisorNumber);
