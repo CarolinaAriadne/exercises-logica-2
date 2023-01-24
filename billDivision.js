9e031898f854cd2039ab8e53335b8355670e1af4
CarolinaAriadne <c.ariadneadv@gmail.com>
2023-01-24 16:57:27 -0300

    exercise

--- billDivision.js
+++ billDivision.js
@@ -17,3 +17,22 @@ function bonAppetit(g, i, d) {
 }
 
 bonAppetit(gastos, index, divisaoTotal);
+
+// function bonAppetit(bill, k, b) {
+//   let sum = 0;
+//   let final = 0;
+
+//   bill.forEach((price) => {
+//     return (sum += price);
+//   });
+//   sum = sum - bill[k];
+//   final = sum / 2;
+
+//   if (final != b) {
+//     console.log(b - final);
+//   } else {
+//     console.log(`Bon Appetit`);
+//   }
+// }
+
+// console.log(bonAppetit([72, 53, 60, 66, 90, 62, 12, 31, 36, 94], 6, 288));
--- dayOfTheProgramer.js
+++ dayOfTheProgramer.js
@@ -47,3 +47,31 @@ function verifyDateProgramer() {
     console.log(`${dayProgramer2}.09.${year}`);
   }
 }
+
+/////////////////////////////////////////////
+
+// function dayOfProgramer(year) {
+//   if (year >= 1700 && year <= 2700) {
+//     if (year >= 1700 && year <= 1917) {
+//       if (year % 4 === 0) {
+//         return `12.09.${year}`;
+//       } else {
+//         return `13.09.${year}`;
+//       }
+//     }
+
+//     if (year === 1918) {
+//       return `26.09.${year}`;
+//     }
+
+//     if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+//       return `12.09.${year}`;
+//     } else {
+//       return `13.09.${year}`;
+//     }
+//   } else {
+//     return `Please, input  a year between 1700 and 2700`;
+//   }
+// }
+
+// console.log(dayOfProgramer(2016));
